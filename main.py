9bb2348fb60bef395e9eaa733a43286c0f4d9ec1
Han Xu <52267740+hanna-xu@users.noreply.github.com>
2020-08-03 13:52:48 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -12,21 +12,15 @@ from model import Model
 
 # IQA_model = './IQA/models/nr_tid_weighted.model'
 
-data1_path = '/home/jxy/xh/project/sl/vis_ir_dataset64.h5'
-data2_path = '/home/jxy/xh/project/sl/oe_ue_Y_dataset64.h5'
-data3_path = '/home/jxy/xh/project/sl/far_near_Y_dataset64.h5'
+data1_path = './vis_ir_dataset64.h5'
+data2_path = './oe_ue_Y_dataset64.h5'
+data3_path = './far_near_Y_dataset64.h5'
 
 patch_size = 64
 LAM = 0 #80000
 LAM_str = '0'
 NUM = 30
 
-# vis_ir: W_EN=12, C=13
-# oe_ue: W_EN=16, C=7
-# far_near: W_EN=11, C=1
-
-# W_EN = [12, 16, 11]
-# C = [13, 7, 1]
 EPOCHES = [3, 2, 2]
 c = [3200, 3500, 100]
 
@@ -36,7 +30,7 @@ def main():
 		# for i in model.var_list:
 		# 	print(i.name)
 
-		saver = tf.train.Saver(var_list = model.var_list, max_to_keep = 80)
+		saver = tf.train.Saver(var_list = model.var_list, max_to_keep = 10)
 
 		tf.summary.scalar('content_Loss', model.content_loss)
 		tf.summary.scalar('ssim_Loss', model.ssim_loss)
@@ -65,56 +59,56 @@ def main():
 
 
 		'''task2'''
-		# num_imgs = source_data1.shape[0]
-		# n_batches1 = int(num_imgs // model.batchsize)
-		# model.step = n_batches1 * EPOCHES[0]
-		# print('model step:', model.step)
-		#
-		# print('Begin to train the network on task2...\n')
-		# saver.restore(sess, './models_save/lam' + LAM_str + '/task1/' + str(n_batches1 * EPOCHES[0]) + '/' + str(
-		# 	n_batches1 * EPOCHES[0]) + '.ckpt')
-		# model.compute_fisher([source_data1], c, sess, num_samples = NUM)
+		num_imgs = source_data1.shape[0]
+		n_batches1 = int(num_imgs // model.batchsize)
+		model.step = n_batches1 * EPOCHES[0]
+		print('model step:', model.step)
 
-		# with tf.device('/cpu:0'):
-		# 	source_data2 = h5py.File(data2_path, 'r')
-		# 	source_data2 = source_data2['data'][:]
-		# 	source_data2 = np.transpose(source_data2, (0, 3, 2, 1))
-		# 	print("source_data2 shape:", source_data2.shape)
+		print('Begin to train the network on task2...\n')
+		saver.restore(sess, './models_save/lam' + LAM_str + '/task1/' + str(n_batches1 * EPOCHES[0]) + '/' + str(
+			n_batches1 * EPOCHES[0]) + '.ckpt')
+		model.compute_fisher([source_data1], c, sess, num_samples = NUM)
 
-		# writer2 = tf.summary.FileWriter("logs/lam" + LAM_str + "/plot_2", sess.graph)
-		# model.star()
-		# train_task(model = model, sess = sess, merged = merged, writer = [writer1, writer2], c=c,
-		#            validset = [source_data1], saver = saver, trainset = source_data2, save_path = './models/lam' + LAM_str+ '/task2/', lam = LAM, task_ind = 2,
-		#            EPOCHES = EPOCHES[1])
+		with tf.device('/cpu:0'):
+			source_data2 = h5py.File(data2_path, 'r')
+			source_data2 = source_data2['data'][:]
+			source_data2 = np.transpose(source_data2, (0, 3, 2, 1))
+			print("source_data2 shape:", source_data2.shape)
+
+		writer2 = tf.summary.FileWriter("logs/lam" + LAM_str + "/plot_2", sess.graph)
+		model.star()
+		train_task(model = model, sess = sess, merged = merged, writer = [writer1, writer2], c=c,
+		           validset = [source_data1], saver = saver, trainset = source_data2, save_path = './models/lam' + LAM_str+ '/task2/', lam = LAM, task_ind = 2,
+		           EPOCHES = EPOCHES[1])
 
 
 
 		'''task3'''
-		# num_imgs = source_data2.shape[0]
-		# n_batches2 = int(num_imgs // model.batchsize)
-		# model.step += n_batches2 * EPOCHES[1]
-		# print('model step:', model.step)
-		# print('Begin to train the network on task3...\n')
-		# saver.restore(sess, './models_save/lam' + LAM_str + '/task2/' + str(n_batches2 * EPOCHES[1]) + '/' + str(
-		# 	n_batches2 * EPOCHES[1]) + '.ckpt')
-		# model.compute_fisher([source_data1, source_data2], c, sess, num_samples = NUM)
-		# for v in range(len(model.F_accum)):
-		# 	print(model.F_accum[v])
-		# with tf.device('/cpu:0'):
-		# 	source_data3 = h5py.File(data3_path, 'r')
-		# 	source_data3 = source_data3['data'][:]
-		# 	source_data3 = np.transpose(source_data3, (0, 3, 2, 1))
-		# 	print("source_data3 shape:", source_data3.shape)
-		#
-		# writer3 = tf.summary.FileWriter("logs/lam" + LAM_str + "/plot_3", sess.graph)
-		# model.star()
-		# train_task(model = model, sess = sess, merged = merged, writer = [writer1, writer2, writer3],
-		#            validset = [source_data1, source_data2], saver = saver, c=c,
-		#            trainset = source_data3, save_path = './models/lam' + LAM_str + '/task3/', lam = LAM, task_ind = 3,
-		#            EPOCHES = EPOCHES[2])
+		num_imgs = source_data2.shape[0]
+		n_batches2 = int(num_imgs // model.batchsize)
+		model.step += n_batches2 * EPOCHES[1]
+		print('model step:', model.step)
+		print('Begin to train the network on task3...\n')
+		saver.restore(sess, './models_save/lam' + LAM_str + '/task2/' + str(n_batches2 * EPOCHES[1]) + '/' + str(
+			n_batches2 * EPOCHES[1]) + '.ckpt')
+		model.compute_fisher([source_data1, source_data2], c, sess, num_samples = NUM)
+		for v in range(len(model.F_accum)):
+			print(model.F_accum[v])
+		with tf.device('/cpu:0'):
+			source_data3 = h5py.File(data3_path, 'r')
+			source_data3 = source_data3['data'][:]
+			source_data3 = np.transpose(source_data3, (0, 3, 2, 1))
+			print("source_data3 shape:", source_data3.shape)
 
-		# saver.restore(sess, './models_save/lam' + LAM_str + '/task3/852/852.ckpt')
-		# model.compute_fisher([source_data1, source_data2, source_data3], c, sess, num_samples = NUM)
+		writer3 = tf.summary.FileWriter("logs/lam" + LAM_str + "/plot_3", sess.graph)
+		model.star()
+		train_task(model = model, sess = sess, merged = merged, writer = [writer1, writer2, writer3],
+		           validset = [source_data1, source_data2], saver = saver, c=c,
+		           trainset = source_data3, save_path = './models/lam' + LAM_str + '/task3/', lam = LAM, task_ind = 3,
+		           EPOCHES = EPOCHES[2])
+
+		saver.restore(sess, './models_save/lam' + LAM_str + '/task3/852/852.ckpt')
+		model.compute_fisher([source_data1, source_data2, source_data3], c, sess, num_samples = NUM)
 
 if __name__ == '__main__':
 	main()
