db88f84a6e52249a12192d560649cf01fc1e10e8
Han Xu <52267740+hanna-xu@users.noreply.github.com>
2022-05-18 16:12:28 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -50,56 +50,94 @@ path2 = path + 'ir/'
 # path2 = path + 'near/'
 
 
+def listdir(path):
+	list_name=[]
+	for file in os.listdir(path):
+		file_path = os.path.join(path, file)
+		if os.path.isdir(file_path):
+			os.listdir(file_path, list_name)
+		else:
+			list_name.append(file_path)
+	return list_name
+
+def rgb2ycbcr(img_rgb):
+	R = img_rgb[:, :, 0]
+	G = img_rgb[:, :, 1]
+	B = img_rgb[:, :, 2]
+	Y = 0.299 * R + 0.587 * G + 0.114 * B
+	Cb = -0.1687 * R - 0.3313 * G + 0.5 * B + 128 / 255.0
+	Cr = 0.5 * R - 0.4187 * G - 0.0813 * B + 128 / 255.0
+	return Y, Cb, Cr
+
+
+def ycbcr2rgb(Y, Cb, Cr):
+    R = Y + 1.402 * (Cr - 128 / 255.0)
+    G = Y - 0.34414 * (Cb - 128 / 255.0) - 0.71414 * (Cr - 128 / 255.0)
+    B = Y + 1.772 * (Cb - 128 / 255.0)
+    R = np.expand_dims(R, axis=-1)
+    G = np.expand_dims(G, axis=-1)
+    B = np.expand_dims(B, axis=-1)
+    return np.concatenate([R, G, B], axis=-1)
+
 
 
 def main():
-	print('\nBegin to generate pictures ...\n')
-	Format='.png'
-	for i in range(10):
-		file_name1 = path1 + str(i + 1) + Format
-		file_name2 = path2 + str(i + 1) + Format
+    print('\nBegin to generate pictures ...\n')
+    Format = '.png'
+    
+    files = listdir(path1)
+    time_cost = np.ones([len(files)], dtype=float)
 
-		img1 = imread(file_name1) / 255.0
-		img2 = imread(file_name2) / 255.0
-		print('file1:', file_name1)
-		print('file2:', file_name2)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
-		Shape1 = img1.shape
-		h1 = Shape1[0]
-		w1 = Shape1[1]
-		Shape2 = img2.shape
-		h2 = Shape2[0]
-		w2 = Shape2[1]
-		assert (h1 == h2 and w1 == w2), 'Two images must have the same shape!'
-		print('input shape:', img1.shape)
-		img1 = img1.reshape([1, h1, w1, 1])
-		img2 = img2.reshape([1, h1, w1, 1])
+    pic_num = 0
 
-		with tf.Graph().as_default(), tf.Session() as sess:
-			M = Model(BATCH_SIZE=1, INPUT_H=h1, INPUT_W=w1, is_training=False)
-			# restore the trained model and run the style transferring
-			t_list = tf.trainable_variables()
-			saver = tf.train.Saver(var_list = t_list)
-			model_save_path = MODEL_SAVE_PATH
-			print(model_save_path)
-			sess.run(tf.global_variables_initializer())
-			saver.restore(sess, model_save_path)
-			outputs = sess.run(M.generated_img, feed_dict = {M.SOURCE1: img1, M.SOURCE2: img2})
-			output = outputs[0, :, :, 0] # 0-1
+    with tf.Graph().as_default(), tf.Session() as sess:
+        M = Model(BATCH_SIZE=1, INPUT_H=None, INPUT_W=None, is_training=False)
+        # restore the trained model and run the style transferring
+        t_list = tf.trainable_variables()
+        saver = tf.train.Saver(var_list=t_list)
+        model_save_path = MODEL_SAVE_PATH
+        print(model_save_path)
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess, model_save_path)
 
-			fig = plt.figure()
-			f1 = fig.add_subplot(311)
-			f2 = fig.add_subplot(312)
-			f3 = fig.add_subplot(313)
-			f1.imshow(img1, cmap = 'gray')
-			f2.imshow(img2, cmap = 'gray')
-			f3.imshow(output, cmap = 'gray')
-			plt.show()
+        for file in files:
+            pic_num += 1
+            name = file.split('/')[-1]
+            name = name.split('.')[-2]
+            print("\033[0;33;40m[" + str(pic_num) + "/" + str(len(files)) + "]: " + name + ".png" + "\033[0m")
 
-			if not os.path.exists(output_path):
-				os.makedirs(output_path)
-			imsave(output_path + str(i + 1) + Format, output)
+            img1 = imread(path1 + file.split('/')[-1], flatten=False) / 255.0
+            img2 = imread(path2 + file.split('/')[-1], flatten=False) / 255.0
 
+            Shape1 = img1.shape
+            print("shape1:", Shape1)
+            if len(Shape1) > 2:
+                img1, img1_cb, img1_cr = rgb2ycbcr(img1)
+            h1 = Shape1[0]
+            w1 = Shape1[1]
+            Shape2 = img2.shape
+            h2 = Shape2[0]
+            w2 = Shape2[1]
+            print("shape2:", Shape2)
+            assert (h1 == h2 and w1 == w2), 'Two images must have the same shape!'
+            img1 = img1.reshape([1, h1, w1, 1])
+            img2 = img2.reshape([1, h1, w1, 1])
+
+
+            start = time.time()
+            outputs = sess.run(M.generated_img, feed_dict={M.SOURCE1: img1, M.SOURCE2: img2})
+            output = outputs[0, :, :, 0]
+            if len(Shape1)>2:
+                output = ycbcr2rgb(output, img1_cb, img1_cr)
+            end = time.time()
+            time_cost[pic_num]=end-start
+            print("Testing [%d] success,Testing time is [%f]\n" % (pic_num, end - start))
+            imsave(output_path + name + Format, output)
+
+    scio.savemat(output_path + '/time.mat', {'T':time_cost})
 
 
 if __name__ == '__main__':
